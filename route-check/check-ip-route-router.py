5d6bf2d9eae9ff159435e4a9efa9937d822ec14d
M.Suranga Jayalath <56903228+surangajayalath@users.noreply.github.com>
2023-06-04 23:45:56 +0530

    Update and rename check-ip-route.py to check-ip-route-router.py

--- route-check/check-ip-route-router.py
+++ route-check/check-ip-route-router.py
@@ -5,31 +5,41 @@ from telnetlib import Telnet
 from ansible.inventory.manager import InventoryManager
 from ansible.parsing.dataloader import DataLoader
 
+# Create a DataLoader instance
 loader = DataLoader()
+
+# Create an InventoryManager instance and load inventory from '/etc/ansible/hosts'
 inventory = InventoryManager(loader=loader, sources=['/etc/ansible/hosts'])
 
+# Iterate over hosts in the 'routers' group
 for host in inventory.get_hosts('routers'):
+    # Get the Ansible variables for the host
     ansible_host = host.get_vars()['ansible_host']
     ansible_user = host.get_vars()['ansible_user']
     ansible_password = host.get_vars()['ansible_password']
     enable_password = host.get_vars()['ansible_enable']
 
+    # Define a function to perform Telnet and show IP routes
     def show_users_telnet():
+        # Create a Telnet instance for the host
         tn = Telnet(ansible_host)
 
+        # Perform Telnet authentication
         tn.read_until(b'Username: ')
         tn.write(ansible_user.encode('ascii') + b'\n')
 
         tn.read_until(b'Password: ')
         tn.write(ansible_password.encode('ascii') + b'\n')
 
+        # Execute 'show ip route' command
         tn.read_until(b'#')
         tn.write(b'show ip route\n')
 
+        # Read the command output and decode it as UTF-8
         stdout = tn.read_until(b'#').decode('utf-8')
         lines = stdout.split('\n')
 
-        # Print the lines directly
+        # Print each line of the output
         for line in lines:
             print(line)
 
