820fa4a9b49cdf8721b316be1aa1dd35a428dd07
M.Suranga Jayalath <56903228+surangajayalath@users.noreply.github.com>
2023-06-05 19:57:49 +0530

    Update check-password-encryption-switch.py

--- check-password/check-password-encryption-switch.py
+++ check-password/check-password-encryption-switch.py
@@ -5,53 +5,71 @@ from telnetlib import Telnet
 from ansible.inventory.manager import InventoryManager
 from ansible.parsing.dataloader import DataLoader
 
-# Create a DataLoader instance
+# Create a DataLoader object to load the inventory data
 loader = DataLoader()
 
-# Create an InventoryManager instance and load inventory from '/etc/ansible/hosts'
+# Create an InventoryManager object with the loader and specify the inventory file
 inventory = InventoryManager(loader=loader, sources=['/etc/ansible/hosts'])
 
-# Iterate over hosts in the 'switch' group
+# Iterate over each host in the 'routers' group of the inventory
 for host in inventory.get_hosts('switch'):
-    # Get the Ansible variables for the host
+    # Get the necessary variables for Telnet connection from the host
     ansible_host = host.get_vars()['ansible_host']
     ansible_user = host.get_vars()['ansible_user']
     ansible_password = host.get_vars()['ansible_password']
     enable_password = host.get_vars()['ansible_enable']
 
-    # Define a function to perform Telnet and show password encryption status
+    # Define a function to connect to the host using Telnet and retrieve information
     def show_users_telnet():
-        # Create a Telnet instance for the host
+        # Connect to the host using Telnet
         tn = Telnet(ansible_host)
 
-        # Perform Telnet authentication
+        # Wait for the 'Username:' prompt and send the ansible_user
         tn.read_until(b'Username: ')
         tn.write(ansible_user.encode('ascii') + b'\n')
 
+        # Wait for the 'Password:' prompt and send the ansible_password
         tn.read_until(b'Password: ')
         tn.write(ansible_password.encode('ascii') + b'\n')
 
-        # Execute 'show run | include service password-encryption' command
+        # Wait for the command prompt and send the command to show password encryption status
         tn.read_until(b'#')
         tn.write(b'show run | include service password-encryption\n')
 
-        # The decode('utf-8') part is used to convert the bytes received from the Telnet connection into a human-readable string format using the UTF-8 encoding.
+        # Read the output until the command prompt and decode it as UTF-8
         stdout = tn.read_until(b'#').decode('utf-8')
+
+        # Split the output into lines
         lines = stdout.split('\n')
 
+        # Print a separator line
         print("----------------------------------------------------------")
 
-        # Check if 'no service password-encryption' line exists in the output
+        # Iterate over each line in the output
         for line in lines:
+            # Check if the line contains the text 'no service password-encryption'
             if 'no service password-encryption' in line:
-                print("Password encryption is not available.")
+                print("Password encryption is not available. Enabling it...")
+                enable_password_encryption(tn)
                 break
         else:
+            # If the loop completes without encountering the 'break' statement,
+            # it means that password encryption is enabled
             print("Password encryption is enabled.")
             print(stdout)
 
-    # Call the function and get the output
-    up_lines = show_users_telnet()
+    def enable_password_encryption(tn):
 
-    # Extract the list of open ports from the output
-    ports = [line.split()[0] for line in up_lines] if up_lines else []
+        # Send the command to enable password encryption
+        tn.write(b'configure terminal\n')
+        tn.read_until(b'#')
+        tn.write(b'service password-encryption\n')
+        tn.read_until(b'#')
+
+
+        # Print a message indicating successful encryption
+        print("Password encryption enabled successfully.")
+
+    # Call the function to retrieve the output from the Telnet connection
+    show_users_telnet()
+
